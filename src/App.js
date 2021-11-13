cbf7b2119f43f88a3603689ff0018937f0ba78c4
mg <madangopalsuthar2010@gmail.com>
2021-11-13 14:48:59 +0530

    frist add

--- src/App.js
+++ src/App.js
@@ -1,24 +1,16 @@
-import logo from './logo.svg';
 import './App.css';
+import Home from './Home';
 
+import Mydata from './Mydata';
+import {Route, Routes} from 'react-router-dom'
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <Routes>
+    <Route exact path="/" element={<Home/>}/>
+      <Route path="/mydata" element={<Mydata/>} />
+    </Routes>
+    </>
   );
 }
 
